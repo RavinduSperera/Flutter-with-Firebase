1cb70ae327d9c46a4de55878613a8cd5a74c6a0c
RavinduPerera <it22064622@my.sliit.lk>
2025-10-06 11:58:11 +0530

    user model is done now anony. user appears in firebase console when clicked the button.

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,9 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth_app/models/UserModel.dart';
 
 // handle all the authenticaton related things
 class AuthServices {
   // instance for firebase auth
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create a user from firebase user with UID
+  UserModel? _userWithFirebaseUserId(User? user) {
+    return user != null ? UserModel(uid: user.uid) : null;
+  }
 
   // sign in anonymously
   Future signInAnonylously() async {
@@ -11,7 +17,7 @@ class AuthServices {
       UserCredential result = await _auth.signInAnonymously();
       User? user =
           result.user; // storing as a firebase user - coud be null too (User?)
-      return user;
+      return _userWithFirebaseUserId(user);
     } catch (err) {
       print(err.toString());
       return null;
