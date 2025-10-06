773cd3962493962f50e792096b4af9a3fdececbb
RavinduPerera <it22064622@my.sliit.lk>
2025-10-06 11:07:29 +0530

    set the onpress function to sign in anony. we can get user id and stuff next going to  create usermodel

--- lib/screens/authentication/login.dart
+++ lib/screens/authentication/login.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth_app/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -8,12 +9,23 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  // reference for the AuthService class
+  final AuthServices _auth = AuthServices();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Login")),
       body: ElevatedButton(
-        onPressed: () {},
+        onPressed: () async {
+          dynamic result = await _auth.signInAnonylously();
+
+          if (result == Null) {
+            print("Error in Sign in ANony");
+          } else {
+            print("Sign in Anony");
+            print(result.uid);
+          }
+        },
         child: Text("Sign in Anonymously"),
       ),
     );
